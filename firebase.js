339af12b3fe14416429156da2825195240085c2a
Yasa Zaheen <yasazaheen@gmail.com>
2022-07-14 19:34:54 +0600

    Build the authentication

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Imports
+import firebase from "firebase";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCw8AxLlbq1pzBYlMeDatupwm_B6WfQuJ8",
+  authDomain: "arcturus-4da6f.firebaseapp.com",
+  projectId: "arcturus-4da6f",
+  storageBucket: "arcturus-4da6f.appspot.com",
+  messagingSenderId: "270600795310",
+  appId: "1:270600795310:web:fe29541649cd869005293f",
+};
+
+// Initialize Firebase
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+export { app };
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -2,12 +2,42 @@
 import React from "react";
 
 // React-native
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+// Expo
+import * as WebBrowser from "expo-web-browser";
+import { ResponseType } from "expo-auth-session";
+import * as Google from "expo-auth-session/providers/google";
+
+// Firebase
+import firebase from "firebase";
 
 // Components
 import SolidButton from "../components/SolidButton";
 
+WebBrowser.maybeCompleteAuthSession();
+
 const WelcomeScreen = () => {
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+    clientId:
+      "270600795310-1948j8khll0gnkl7kki5jom6p4ut5l4u.apps.googleusercontent.com",
+  });
+
+  React.useEffect(() => {
+    if (response?.type === "success") {
+      const { id_token } = response.params;
+
+      const auth = firebase.auth();
+      const provider = new firebase.auth.GoogleAuthProvider();
+      const credential = provider.credential(id_token);
+      auth.signInWithCredential(credential);
+    }
+  }, [response]);
+
+  const signInHandler = () => {
+    promptAsync();
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Hey there!</Text>
@@ -15,7 +45,7 @@ const WelcomeScreen = () => {
         Don’t be shy, click the button below to start chatting with your friends
         (it’s free 😉)!
       </Text>
-      <SolidButton text="Sign in with Google" />
+      <SolidButton text="Sign in with Google" onPress={signInHandler} />
       <Text style={styles.mastHead}>Made by Yasa Zaheen</Text>
     </View>
   );
